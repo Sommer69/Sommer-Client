7132e53119e12c90c1a11debc39ba4913be16396
Sommer69 <98322964+Sommer69@users.noreply.github.com>
2022-12-08 18:35:17 +0100

    Update Category.java

--- src/main/java/me/vene/skilled/modules/Category.java
+++ src/main/java/me/vene/skilled/modules/Category.java
@@ -7,13 +7,13 @@ public enum Category
     G(StringRegistry.register(""), 7), 
     C(StringRegistry.register(new String(new char[] { 'C', 'o', 'm', 'b', 'a', 't' })), 0), 
     O(StringRegistry.register(new String(new char[] { 'O', 't', 'h', 'e', 'r' })), 2), 
-    P(StringRegistry.register("Player"), 3), 
-    R(StringRegistry.register("Render"), 4), 
-    U(StringRegistry.register("Utility"), 5), 
-    CO(StringRegistry.register("Commands"), 8), 
+    P(StringRegistry.register("Spiller"), 3), 
+    R(StringRegistry.register("Visuelle ting"), 4), 
+    U(StringRegistry.register("Smarte ting"), 5), 
+    CO(StringRegistry.register("Kommandoer"), 8), 
     A(StringRegistry.register(new String(new char[] { 'T', 'e', 'x', 't', ' ', 'G', 'U', 'I' })), 1), 
-    I(StringRegistry.register("Warnings"), 6), 
-    F(StringRegistry.register("Friends"), 9);
+    I(StringRegistry.register("Advarsler"), 6), 
+    F(StringRegistry.register("Venner"), 9);
     
     private String name;
     private int id;
